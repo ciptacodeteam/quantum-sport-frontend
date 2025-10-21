a467519bbf7ae4159bb7f42584bd4042ed6a81d1
Juan Antonio <juananthonio23@gmail.com>
2025-10-21 12:55:17 +0700

    feat: add image configuration to allow loading from local backend server

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // Allow loading images from the local backend server
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: 'storages/**'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
