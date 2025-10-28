bb1077610a4b7c430c916aa4dd06969702ed942e
Juan Antonio <juananthonio23@gmail.com>
2025-10-28 20:57:53 +0700

    feat: update MembershipCtaSection with new image and layout adjustments

--- components/section/home/MembershipCtaSection.tsx
+++ components/section/home/MembershipCtaSection.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image';
 const MembershipCtaSection = () => {
   return (
     <section className="mx-auto my-12 max-w-7xl px-4">
-      <div className="bg-primary rounded-lg p-6 text-center md:p-10">
-        <div className="grid grid-cols-2 items-center gap-6">
-          <div className="flex flex-col items-start">
+      <div className="bg-primary rounded-lg text-center">
+        <div className="grid max-h-60 grid-cols-1 place-content-center items-center gap-6 sm:grid-cols-2">
+          <div className="relative z-10 flex flex-col items-start p-6 md:p-10">
             <h2 className="text-left text-2xl font-semibold text-white md:text-3xl">
-              Gabung <br className="lg:hidden" />
+              Gabung <br className="hidden sm:block lg:hidden" />
               Membership <br /> Sekarang!
             </h2>
 
@@ -19,18 +19,19 @@ const MembershipCtaSection = () => {
             <span className="text-muted mt-4 text-left text-xs">
               Dapatkan prioritas booking dan promo menarik lainnya!
             </span>
+            <div className="absolute top-1/2 right-10 block h-[270px] w-3 -translate-y-1/2 -rotate-16 rounded bg-white sm:hidden md:right-2/4 md:h-[300px]"></div>
           </div>
-          <div className="relative">
-            <div className="right absolute top-1/2 right-1/4 h-[270px] w-3 -translate-y-1/2 -rotate-16 rounded bg-white md:right-2/4 md:h-[300px]"></div>
+          <div className="relative hidden items-center justify-end sm:flex">
+            <div className="absolute top-1/2 right-40 h-[270px] w-3 -translate-y-1/2 -rotate-16 rounded bg-white md:h-[300px]"></div>
 
-            <div className="absolute top-1/2 -right-16 w-[290px] -translate-y-1/2 lg:-right-10 lg:w-[320px]">
+            <div className="relative">
               <Image
-                src="/assets/img/player-membership.png"
+                src="/assets/img/player.png"
                 alt="Membership Cta"
                 preload
-                width={400}
-                height={400}
-                className="w-full rotate-12 object-contain"
+                width={500}
+                height={500}
+                className="w-auto min-w-[280px] object-contain md:h-80"
               />
             </div>
           </div>
