c8bfe81197c607b4ac99224900be3b0ae27a84b8
Juan Antonio <juananthonio23@gmail.com>
2025-11-07 23:04:59 +0700

    refactor: replace Button with InputGroupButton in PasswordInput component and improve styling

--- components/ui/password-input.tsx
+++ components/ui/password-input.tsx
@@ -2,38 +2,39 @@
 
 import * as React from 'react';
 
-import { Button } from '@/components/ui/button';
 import { Input, type InputProps } from '@/components/ui/input';
 import { cn } from '@/lib/utils';
 import { IconEye, IconEyeOff } from '@tabler/icons-react';
+import { InputGroup, InputGroupButton } from './input-group';
 
 const PasswordInput = ({ className, ...props }: InputProps) => {
   const [showPassword, setShowPassword] = React.useState(false);
-  const disabled = props.value === '' || props.value === undefined || props.disabled;
 
   return (
-    <div className="relative w-full">
+    <InputGroup className="w-full">
       <Input
-        className={cn('hide-password-toggle pr-10', className)}
+        className={cn(
+          'hide-password-toggle rounded-r-none border-r-0 pr-10 shadow-none',
+          className
+        )}
         {...props}
         type={showPassword ? 'text' : 'password'}
       />
-      <Button
+      <InputGroupButton
         type="button"
         variant="ghost"
         size="sm"
-        className="absolute top-0 right-0 z-10 h-full px-3 py-2 hover:bg-transparent"
+        className="z-30 cursor-pointer rounded-full border-l-0 p-0 hover:bg-transparent"
         onClick={() => setShowPassword((prev) => !prev)}
-        disabled={disabled}
       >
-        {showPassword && !disabled ? (
+        {showPassword ? (
           <IconEye className="text-muted-foreground size-5" aria-hidden="true" />
         ) : (
           <IconEyeOff className="text-muted-foreground size-5" aria-hidden="true" />
         )}
         <span className="sr-only">{showPassword ? 'Hide password' : 'Show password'}</span>
-      </Button>
-    </div>
+      </InputGroupButton>
+    </InputGroup>
   );
 };
 
